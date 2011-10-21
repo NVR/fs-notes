ab1f06dccf4ee39e71407acda53ea1b0dab37ee7
NVR <envy.94@mail.ru>
2011-10-21 10:22:50 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FsNotes::Application.config.secret_token = '1a6c9d59a72f096ed3e03fb318416bf0125f0bf278b6ab7252d7543ee28e46fcbc38595328033e7ac6dfc475e8167d7eb640362da82760897fae7b286f8d6278'
