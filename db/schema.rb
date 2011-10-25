6e8d83d8cdaef42f1859e4a1aa0b1c9ed9abb4ec
NVR <envy.94@mail.ru>
2011-10-25 12:10:54 +0400

    formated slim-files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111024084740) do
+ActiveRecord::Schema.define(:version => 20111024154158) do
+
+  create_table "notes", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
