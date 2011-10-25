f44a03d22ec09b277b347b9b90512b6d26b147f6
NVR <envy.94@mail.ru>
2011-10-25 16:49:49 +0400

    reedit files

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,32 +4,26 @@ class NotesController < ApplicationController
     @notes = Note.all
   end
 
-
   def show
     @note = Note.find(params[:id])
   end
-
 
   def new
     @note = Note.new
   end
 
-
   def edit
     @note = Note.find(params[:id])
-    @title =  'Edit note'
+    @title = 'Edit note'
   end
-
 
   def create
     @note = Note.new(params[:note])
     respond_to do |format|
       if @note.save
         format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render json: @note, status: :created, location: @note }
       else
         format.html { render action: "new" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,10 +33,8 @@ class NotesController < ApplicationController
     respond_to do |format|
       if @note.update_attributes(params[:note])
         format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-        format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -50,10 +42,8 @@ class NotesController < ApplicationController
   def destroy
     @note = Note.find(params[:id])
     @note.destroy
-
     respond_to do |format|
       format.html { redirect_to notes_url }
-      format.json { head :ok }
     end
   end
 end
