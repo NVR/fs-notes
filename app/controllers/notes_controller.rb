fce79cc37bfd0f77ee21c6a7cbb5bb1160a65266
NVR <envy.94@mail.ru>
2011-11-09 16:59:08 +0300

    expose notes,note,topic

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,51 +3,28 @@ class NotesController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
   before_filter :page_exceedance?
   before_filter :can_edit?, :only => [:edit, :delete]
-
-  def page_exceedance?
-    if params[:page].to_i > Note.pages_count
-      params[:page] = (Note.pages_count).to_s
-    end
+  expose :note
+  expose :topic
+  expose(:notes) do
+    notes_with_scope = (params[:topic_id].present?) ? topic.notes : Note.scoped
+    notes_with_scope = notes_with_scope.search(params[:search]) if params[:search].present?
+    notes_with_scope
   end
 
   def index
-    if params[:topic_id].present?
-      @topic = Topic.find(params[:topic_id])
-      @notes = @topic.notes
-      @title = "Notes associated with #{@topic.name}"
-      if params[:search].present?
-        @notes = @notes.search(params[:search])
-        @count = @notes.count
-        @notes = @notes.page(params[:page])
-      else
-        @count = @notes.count
-        @notes = @notes.page(params[:page])
-      end
-    else
-      @title = "Notes"
-      if params[:search].present?
-        @notes = Note.search(params[:search])
-        @count = @notes.count
-        @notes = @notes.page(params[:page])
-      else
-        @notes = Note.page(params[:page])
-        @count = Note.count
-      end
-    end
+    @count = notes.count
+    @notes = notes.page(params[:page])
   end
 
   def show
-    @note = Note.find(params[:id])
   end
 
   def new
     @preview = false
-    @note = Note.new
     @title = 'New note'
   end
 
   def edit
-    @note = Note.find(params[:id])
     @title = 'Edit note'
     @preview = false
   end
@@ -71,17 +48,16 @@ class NotesController < ApplicationController
   end
 
   def update
-    @note = Note.find(params[:id])
     if params[:commit] == "Preview"
-      @note.update_attributes(params[:note])
+      note.update_attributes(params[:note])
       @preview = true
       render action: "edit"
     elsif params[:commit] == "Back"
       @preview = false
       render action: "edit"
     else
-      if @note.update_attributes(params[:note])
-        redirect_to @note, notice: 'Note was successfully updated.'
+      if note.update_attributes(params[:note])
+        redirect_to note, notice: 'Note was successfully updated.'
       else
         render action: "edit"
       end
@@ -89,8 +65,7 @@ class NotesController < ApplicationController
   end
 
   def destroy
-    @note = Note.find(params[:id])
-    @note.destroy
+    note.destroy
     redirect_to notes_url
   end
 
@@ -100,15 +75,20 @@ class NotesController < ApplicationController
 
   private
 
-    def can_edit?
-      @note = Note.find(params[:id])
-      unless editable?(@note)
-        redirect_to @note , notice: "You can't edit other people's notes."
-      end
+  def can_edit?
+    unless editable?(note)
+      redirect_to note , notice: "You can't edit other people's notes."
     end
+  end
 
-    def editable?(note)
-      user_signed_in? && (current_user.id == note.user_id)
+  def editable?(note)
+    user_signed_in? && (current_user.id == note.user_id)
+  end
+
+  def page_exceedance?
+    if params[:page].to_i > Note.pages_count
+      params[:page] = (Note.pages_count).to_s
     end
+  end
 
 end
