00ce7920cb0c3f551e2f5138e6ac18d8a7357f8a
NVR <envy.94@mail.ru>
2011-11-14 16:16:11 +0300

    redirecting on not existing topic or notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper_method :existing_topics
+  rescue_from ActiveRecord::RecordNotFound, :with => :someredirect
 
   def existing_topics
      Topic.all
   end
-  
+
+  def someredirect
+    redirect_to root_path
+  end
+
 end
