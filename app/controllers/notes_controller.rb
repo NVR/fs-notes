b2388b8c1f16bae61bdf2aef8bb5710063facb7a
NVR <envy.94@mail.ru>
2011-11-15 18:51:08 +0300

    deleted button from edit user form+ limited amount of notes on show page+ added user_notes_path

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,7 +9,8 @@ class NotesController < ApplicationController
   expose :topic
   expose :user
   expose(:notes) do
-    notes_with_scope = (params[:topic_id].present?) ? topic.notes : Note.scoped
+    notes_with_scope = (params[:topic_id].present?) ? topic.notes : Note.scoped.order("created_at  DESC")
+    Rails.logger.debug
     notes_with_scope = user.notes if params[:user_id].present?
     notes_with_scope = notes_with_scope.search(params[:search]) if params[:search].present?
     notes_with_scope
