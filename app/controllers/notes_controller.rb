b482e8d8733c8e107d79fd7ed41af95674aa7bbb
NVR <envy.94@mail.ru>
2011-11-16 14:08:43 +0300

    fixed user search,man

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -11,7 +11,7 @@ class NotesController < ApplicationController
   expose(:notes) do
     notes_with_scope = (params[:topic_id].present?) ? topic.notes : Note.scoped.order("created_at  DESC")
     Rails.logger.debug
-    notes_with_scope = user.notes if params[:user_id].present?
+    notes_with_scope = notes_with_scope.user_notes(params[:user_id]) if params[:user_id].present?
     notes_with_scope = notes_with_scope.search(params[:search]) if params[:search].present?
     notes_with_scope
   end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,9 +1,15 @@
 class Note < ActiveRecord::Base
 
-  scope :search,->(search) { 
+  scope :search,->(search,user_id = '') { 
     search_condition = "%" + search + "%"
-    where('title LIKE ? OR body LIKE ?', search_condition, search_condition)
+    userid_formatted = (user_id.present? ? "%" + user_id + "%" : "")
+    where('title LIKE ? OR body LIKE ? AND user_id LIKE ?', search_condition, search_condition, userid_formatted )
   } 
+  scope :user_notes,->(user_id){
+    userid_formatted = "%" + user_id + "%"
+    where('user_id LIKE ?', userid_formatted)
+  }
+
 
   PER_PAGE = 9
 
